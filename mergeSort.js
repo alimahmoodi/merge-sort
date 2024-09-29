da6e221bc7fdf2d01a4744280b3bac9a3d27fd32
ali <alimahmudi.alami@gmail.com>
2024-09-30 00:55:00 +0330

    first commit

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const arr = ["nastaran", "shayan", "ahmad", "niloofar"];
+
+const sorted = mergeSort(arr);
+
+console.log(sorted[sorted.length - 2]);
